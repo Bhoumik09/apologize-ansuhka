0e73fd6c8c886f2ec6c94e25de38a7ea2f4dca40
Bhoumik <Bhoumikchopra12@gmail.com>
2024-01-07 15:16:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Create an audio element
+        var audio = new Audio('Tere-Jaisa-Yaar-Kahan(PaglaSongs).mp3');
+
+        // Set attributes for autoplay and loop
+        audio.setAttribute('autoplay', true);
+        audio.setAttribute('loop', true);
+
+        // Append the audio element to the body
+        document.body.appendChild(audio);
+    });
