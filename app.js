c918c567622100a335dcee4fd746ff846a261012
Bhoumik <Bhoumikchopra12@gmail.com>
2024-01-08 11:30:10 +0530

    audio error corrected

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
     document.addEventListener("DOMContentLoaded", function() {
         // Create an audio element
         var audio = new Audio('Tere-Jaisa-Yaar-Kahan(PaglaSongs).mp3');
-
+       
         // Set attributes for autoplay and loop
         audio.setAttribute('autoplay', true);
         audio.setAttribute('loop', true);
-
+        audio.currentTime=11
         // Append the audio element to the body
         document.body.appendChild(audio);
+        setInterval(()=>{
+            console.log(audio.currentTime)
+        },1000)
+        
     });
+    
